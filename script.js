39afab4e41476beb9035216a028f9ab09d7082c9
467066-sys <467066@wsk.ac.th>
2026-07-12 06:40:37 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+<nav>
+
+<a href="index.html">Home</a>
+<a href="sop.html">SOP</a>
+<a href="whoiam.html">Who I am</a>
+<a href="#">Activities</a>
+<a href="#">Certificates</a>
+<a href="#">Contact</a>
+
+</nav>
+
+<script src="script.js"></script>
+
+</body>
+</html>
